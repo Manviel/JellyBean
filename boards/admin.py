c63770c75496b6d8d459452296e612803ef9e398
Manviel <mihaylo.merezhko@kname.edu.ua>
2018-10-10 19:02:23 +0300

    Avatar for user

--- boards/admin.py
+++ boards/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from accounts.models import User
+from accounts.models import Subject, User
 
 from .models import Board, Topic
 
 admin.site.register(Board)
 admin.site.register(Topic)
 admin.site.register(User)
+admin.site.register(Subject)
