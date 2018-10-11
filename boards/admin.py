bb866811b89c71f7874be682c81efd677f7f19a3
Manviel <mihaylo.merezhko@kname.edu.ua>
2018-10-11 11:56:09 +0300

    Action in admin for hide model

--- boards/admin.py
+++ boards/admin.py
@@ -4,7 +4,25 @@ from accounts.models import User
 
 from .models import Board, Photo, Topic
 
-admin.site.register(Board)
+
+def apply_active(modeladmin, request, queryset):
+    for board in queryset:
+        board.description = ''
+        board.save()
+
+
+apply_active.short_description = 'Apply'
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+    actions = [apply_active, ]
+
+    def get_model_perms(self, request):
+        return {}
+
+
+admin.site.register(Board, BookAdmin)
 admin.site.register(Topic)
 admin.site.register(User)
 admin.site.register(Photo)
